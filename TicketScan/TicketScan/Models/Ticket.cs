be419574f53c3d088f79a26f2e6d13e69d72ab11
Miguel López-N. Alcalde <fmiguel.lopez@um.es>
2019-06-29 13:41:25 +0200

    Initial commit Xamarin app

---
+++ TicketScan/TicketScan/Models/Ticket.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace TicketScan.Models
+{
+    public class Ticket
+    {
+        public Ticket()
+        {
+        }
+    }
+}
---
+++ TicketScan/TicketScan/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using TicketScan.Models;
+
+namespace TicketScan.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
