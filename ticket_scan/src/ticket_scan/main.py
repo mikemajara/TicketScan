09439a60a4502dc0c0397834c1f921c4405d197b
Miguel López-N. Alcalde <fmiguel.lopez@um.es>
2019-08-24 12:31:26 +0200

    Removed venv from commit. Project working and debuggable as before.

---
+++ ticket_scan/src/ticket_scan/main.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+This is a skeleton file that can serve as a starting point for a Python
+console script. To run this script uncomment the following lines in the
+[options.entry_points] section in setup.cfg:
+
+    console_scripts =
+         fibonacci = ticket_scan.skeleton:run
+
+Then run `python setup.py install` which will install the command `fibonacci`
+inside your current environment.
+Besides console scripts, the header (i.e. until _logger...) of this file can
+also be used as template for Python modules.
+
+Note: This skeleton file can be safely removed if not needed!
+"""
+
+from ticket_scan import __version__
+import argparse
+import sys
+import logging
+
+from scanner import slicer, ocr_batch
+
+
+DEFAULT_SCAN = False
+
+
+__author__ = "Miguel López-N. Alcalde"
+__copyright__ = "Miguel López-N. Alcalde"
+__license__ = "proprietary"
+
+_logger = logging.getLogger(__name__)
+
+
+def parse_args(args):
+    """Parse command line parameters
+
+    Args:
+      args ([str]): command line parameters as list of strings
+
+    Returns:
+      :obj:`argparse.Namespace`: command line parameters namespace
+    """
+    parser = argparse.ArgumentParser(
+        description="Just a Fibonnaci demonstration")
+    parser.add_argument(
+        '--version',
+        action='version',
+        version='ticket_scan {ver}'.format(ver=__version__))
+    parser.add_argument("-i", "--image", required=True,
+                        help="Path to the image to be scanned")
+    parser.add_argument("-s", "--scan",
+                        action="store_true",
+                        default=DEFAULT_SCAN,
+                        help="Crop and transform image searching for document form")
+    parser.add_argument(
+        '-v',
+        '--verbose',
+        dest="loglevel",
+        help="set loglevel to INFO",
+        action='store_const',
+        const=logging.INFO)
+    parser.add_argument(
+        '-vv',
+        '--very-verbose',
+        dest="loglevel",
+        help="set loglevel to DEBUG",
+        action='store_const',
+        const=logging.DEBUG)
+    return parser.parse_args(args)
+
+
+def setup_logging(loglevel):
+    """Setup basic logging
+
+    Args:
+      loglevel (int): minimum loglevel for emitting messages
+    """
+    logformat = "[%(asctime)s] %(levelname)s:%(name)s:%(message)s"
+    logging.basicConfig(level=loglevel, stream=sys.stdout,
+                        format=logformat, datefmt="%Y-%m-%d %H:%M:%S")
+
+
+def main(args):
+    """Main entry point allowing external calls
+
+    Args:
+      args ([str]): command line parameter list
+    """
+    args = parse_args(args)
+    setup_logging(args.loglevel)
+    _logger.debug("Starting crazy scanner...")
+
+    path_image = args.image
+
+    if args.scan:
+        pass
+
+    path_output = slicer.slice(path_image, interactive=False)
+
+    ocr_batch.extract_lines_of_text(path_output)
+
+
+def run():
+    """Entry point for console_scripts
+    """
+    main(sys.argv[1:])
+
+
+if __name__ == "__main__":
+    run()
---
+++ ticket_scan/src/ticket_scan/scanner/ocr_batch.py
@@ -0,0 +1,92 @@
+import argparse
+import os
+import cv2
+import json
+import natsort
+from .ocr import extract_text
+
+
+DEFAULT_OEM = 1
+DEFAULT_PSM = 7
+DEFAULT_SIDE_MARGIN = 5
+DEFAULT_FULL_BOX_IMAGE = True
+
+
+def extract_lines_of_text(path,
+                          oem=DEFAULT_OEM,
+                          psm=DEFAULT_PSM,
+                          side_margin=DEFAULT_SIDE_MARGIN,
+                          full_box_image=DEFAULT_FULL_BOX_IMAGE):
+    text_recognition_dict = {}
+    file_list = os.listdir(path)
+    file_list = list(filter(lambda x: x.startswith("cropped"), file_list))
+    file_list = natsort.natsorted(file_list)
+    for file in file_list:
+
+        if file.startswith("cropped"):
+            filepath = os.path.join(path, file)
+            print(filepath)
+            image = cv2.imread(filepath)
+            orig = image.copy()
+
+            text_recognised = extract_text(img=orig,
+                                           oem=oem,
+                                           psm=psm,
+                                           lang="spa",
+                                           full_box_image=full_box_image,
+                                           side_margin=side_margin)
+            text_recognition_dict[file] = text_recognised
+
+    f = open(os.path.join(path, "text_recognition_" +
+                          str(oem) + "_" + str(psm) + ".json"), "w")
+    json.dump(text_recognition_dict, f, ensure_ascii=False, indent=2)
+    f.close()
+
+
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("path",
+                help="Path from which to obtain images to parse")
+ap.add_argument("-f", "--full-box-image",
+                action="store_true",
+                default=DEFAULT_FULL_BOX_IMAGE,
+                help="Take full image as text")
+ap.add_argument("-m", "--side-margin",
+                default=DEFAULT_SIDE_MARGIN,
+                type=int,
+                help="Side margin to crop from image")
+ap.add_argument("-oem", "--oem", type=int, default=1,
+                help="controls the type of algorithm used by Tesseract."
+                     "OCR Engine modes:"
+                     "0    Legacy engine only."
+                     "1    Neural nets LSTM engine only."
+                     "2    Legacy + LSTM engines."
+                     "3    Default, based on what is available.")
+ap.add_argument("-psm", "--psm", type=int, default=7,
+                help="controls the automatic Page Segmentation Mode used by Tesseract"
+                     "Page segmentation modes:"
+                     "0    Orientation and script detection (OSD) only."
+                     "1    Automatic page segmentation with OSD."
+                     "2    Automatic page segmentation, but no OSD, or OCR."
+                     "3    Fully automatic page segmentation, but no OSD. (Default)"
+                     "4    Assume a single column of text of variable sizes."
+                     "5    Assume a single uniform block of vertically aligned text."
+                     "6    Assume a single uniform block of text."
+                     "7    Treat the image as a single text line."
+                     "8    Treat the image as a single word."
+                     "9    Treat the image as a single word in a circle."
+                     "10    Treat the image as a single character."
+                     "11    Sparse text. Find as much text as possible in no particular order."
+                     "12    Sparse text with OSD."
+                     "13    Raw line. Treat the image as a single text line,"
+                     "      bypassing hacks that are Tesseract-specific.")
+
+if __name__ == "__main__":
+    args = vars(ap.parse_args())
+
+    extract_lines_of_text(args["path"],
+                          oem=args["oem"],
+                          psm=args["psm"],
+                          full_box_image=args["full_box_image"],
+                          side_margin=args["side_margin"]
+                          )
