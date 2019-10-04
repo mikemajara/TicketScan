31f78e942d80e8cda83f33a57f9661287657b9d3
Javier Cervantes Villanueva <javiercvilla@gmail.com>
2019-10-04 17:20:36 +0200

    Create directory for uploaded images at startup
    - Creates 'uploaded_images' directory if it does not exist
    - Refactor out the creation of a filepath out of a file
    - Add class attributes for the uploaded_images directory path and the
    file format for new uploaded images

--- ticket_scan/src/ticket_scan/server.py
+++ ticket_scan/src/ticket_scan/server.py
@@ -1,3 +1,4 @@
+import os
 import datetime as dt
 
 from flask import jsonify
@@ -10,22 +11,31 @@ from ticket_scan.scanner.lidl_ticket_parser import LidlTicketParser
 
 
 class Server(Resource):
+
+    UPLOADED_IMAGES_PATH = "../../uploaded_images"
+    FILEPATH_FORMAT = UPLOADED_IMAGES_PATH + "/{timestamp}.{file_extension}"
+
+    def __init__(self):
+        os.makedirs(Server.UPLOADED_IMAGES_PATH, exist_ok=True)
+
+    def _get_filepath(self, file):
+        timestamp = dt.datetime.now().isoformat()
+        file_extension = file.content_type.split("/")[1]
+        filepath = Server.FILEPATH_FORMAT.format(timestamp=timestamp, file_extension=file_extension)
+        return filepath
+
     def post(self):
         parse = reqparse.RequestParser()
         parse.add_argument('file', type=FileStorage,
                            location='files')
 
         args = parse.parse_args()
-
         file = args['file']
+
         result = None
         if file:
-            timestamp = dt.datetime.now().isoformat()
-            #filext = os.path.splitext(file.filename)[1]
-            filext = file.content_type.split("/")[1]
-            filepath = '../../uploaded_images/' + timestamp + "." + filext
+            filepath = self._get_filepath(file)
             file.save(filepath)
-            #path_output = slicer.slice(filepath, interactive=False)
             # TODO: Implemente a dynamic way to instantiate the parser.
             #ticket_parser = LidlTicketParser()
             ticket_parser = MercadonaTicketParser()
