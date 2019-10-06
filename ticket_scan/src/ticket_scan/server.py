f60ed58f116d3e1bf0fd72035eba3fd865d21b1a
Javier Cervantes Villanueva <javiercvilla@gmail.com>
2019-10-06 16:18:40 +0200

    Do not save twice the same image
    - Calculate the hash of every new uploaded image
    - Save only new images

--- ticket_scan/src/ticket_scan/server.py
+++ ticket_scan/src/ticket_scan/server.py
@@ -1,5 +1,8 @@
 import os
+import cv2
+import numpy as np
 import datetime as dt
+import logging
 
 from flask import jsonify
 from flask_restful import Resource, reqparse
@@ -9,19 +12,58 @@ from werkzeug.datastructures import FileStorage
 from ticket_scan.scanner import ocr_batch
 from ticket_scan.scanner.lidl_ticket_parser import LidlTicketParser
 
+logger = logging.getLogger(__name__)
 
 class Server(Resource):
 
     UPLOADED_IMAGES_PATH = "../../uploaded_images"
-    FILEPATH_FORMAT = UPLOADED_IMAGES_PATH + "/{timestamp}.{file_extension}"
+    FILEPATH_FORMAT = UPLOADED_IMAGES_PATH + "/{hash}.{file_extension}"
 
     def __init__(self):
         os.makedirs(Server.UPLOADED_IMAGES_PATH, exist_ok=True)
 
+    def _dhash(self, file, hash_size = 8):
+        # resize the input image, adding a single column (width) so we
+        # can compute the horizontal gradient
+        resized = cv2.resize(file, (hash_size + 1, hash_size))
+
+        # compute the (relative) horizontal gradient between adjacent
+        # column pixels
+        diff = resized[:, 1:] > resized[:, :-1]
+
+        # convert the difference image to a hash
+        return sum([2 ** i for (i, v) in enumerate(diff.flatten()) if v])
+
+    def _get_hash(self, file):
+        # file is read as an image through numpy to avoid saving it to disk
+        # read image file string data
+        filestr = file.read()
+        # file object’s position needs to point to the 0th byte, otherwise it cannot be saved later on
+        file.seek(0)
+        # convert string data to numpy array
+        npimg = np.fromstring(filestr, np.uint8)
+        # convert numpy array to image
+        image = cv2.imdecode(npimg, cv2.IMREAD_COLOR)
+        if image is None:
+            raise Exception("Non existing file: ", file)
+
+        image_blk_white = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        image_hash = self._dhash(image_blk_white)
+        return image_hash
+
     def _get_filepath(self, file):
-        timestamp = dt.datetime.now().isoformat()
         file_extension = file.content_type.split("/")[1]
-        filepath = Server.FILEPATH_FORMAT.format(timestamp=timestamp, file_extension=file_extension)
+        filehash = self._get_hash(file)
+        filepath = Server.FILEPATH_FORMAT.format(hash=filehash, file_extension=file_extension)
+        return os.path.abspath(filepath)
+
+    def save_file(self, file):
+        filepath = self._get_filepath(file)
+        if not os.path.exists(filepath):
+            file.save(filepath)
+            logger.info("New file saved: {}".format(filepath))
+        else:
+            logger.info("Files already exists: {}".format(filepath))
         return filepath
 
     def post(self):
@@ -34,8 +76,7 @@ class Server(Resource):
 
         result = None
         if file:
-            filepath = self._get_filepath(file)
-            file.save(filepath)
+            filepath = self.save_file(file)
             # TODO: Implemente a dynamic way to instantiate the parser.
             #ticket_parser = LidlTicketParser()
             ticket_parser = MercadonaTicketParser()
