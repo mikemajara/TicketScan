49582f582856eea8d72bdbfef31742d1d9b63a95
Miguel López-N. Alcalde <fmiguel.lopez@um.es>
2019-10-08 18:29:54 +0200

    Removed unnecesary code from model. (TypeScript transpiles to the same code) Added PaymentInformation Deleted ProprietaryTicketCode classes. Not used at the moment.

--- TicketScanApp/src/model/Ticket.ts
+++ TicketScanApp/src/model/Ticket.ts
@@ -1,40 +1,17 @@
 import Store from './Store';
-import ProprietaryTicketCode from './ProprietaryTicketCode';
-import TicketLine from './TicketLine';
 import Company from './Company';
+import TicketLine from './TicketLine';
+import PaymentInformation from './PaymentInformation';
 
 class Ticket {
-  _id: string;
-  company: Company;
-  store: Store;
-  datetime: Date;
-  proprietaryTicketCodes: Array<ProprietaryTicketCode>;
-  paymentMethod: string;
-  total: number;
-  returned: number;
-  lines: Array<TicketLine>;
-
   constructor(
-    _id: string,
-    company: Company,
-    store: Store,
-    datetime: Date,
-    proprietaryTicketCodes: Array<ProprietaryTicketCode>,
-    paymentMethod: string,
-    total: number,
-    returned: number,
-    lines: Array<TicketLine>
-  ) {
-    this._id = _id;
-    this.company = company;
-    this.store = store;
-    this.datetime = datetime;
-    this.proprietaryTicketCodes = proprietaryTicketCodes;
-    this.paymentMethod = paymentMethod;
-    this.total = total;
-    this.returned = returned;
-    this.lines = lines;
-  }
+    private _id?: string,
+    private company?: Company,
+    private store?: Store,
+    private datetime?: Date,
+    private paymentInformation?: PaymentInformation,
+    private lines?: Array<TicketLine>,
+  ) { }
 }
 
 export default Ticket;
