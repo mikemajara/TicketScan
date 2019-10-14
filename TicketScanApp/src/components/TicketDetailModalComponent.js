b75f5fb8160dd38f2aa0f8d26666c993c0f72363
Miguel López-N. Alcalde <fmiguel.lopez@um.es>
2019-10-14 10:13:37 +0200

    Installed react-native-modal. Created card style modal with swipedown.

--- TicketScanApp/src/components/TicketDetailModalComponent.js
+++ TicketScanApp/src/components/TicketDetailModalComponent.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
-import { View, Text, TouchableHighlight, Modal, Alert, StyleSheet } from 'react-native';
+import Modal from 'react-native-modal';
+import { View, Text, TouchableHighlight, Alert, StyleSheet } from 'react-native';
 import { iOSColors, iOSUIKit, systemWeights } from 'react-native-typography';
 
 import { styleDebug } from '../helpers';
@@ -9,42 +10,37 @@ export default function TicketLineDetailModal(props) {
   return (
     <Modal
       style={styles.modal}
-      animationType="fade"
-      transparent
-      visible={props.visible}
-      onRequestClose={() => {
-        Alert.alert('Modal has been closed.');
-      }}>
-      <View style={styles.modalBackground}>
-        <View style={styles.container}>
-          <Text>Hello World!</Text>
+      backdropColor="#00000040"
+      onSwipeComplete={props.onPressClose}
+      swipeDirection={['down']}
+      isVisible={props.visible}
+    >
+      <View style={styles.container}>
+        <Text>Hello World!</Text>
 
-          <TouchableHighlight
-            onPress={props.onPressClose}>
-            <Text>Hide Modal</Text>
-          </TouchableHighlight>
-        </View>
+        <TouchableHighlight
+          onPress={props.onPressClose}>
+          <Text>Hide Modal</Text>
+        </TouchableHighlight>
       </View>
     </Modal>
   )
 }
 
 const styles = StyleSheet.create({
-  modalBackground: {
-    flex: 1,
-    alignItems: 'center',
-    flexDirection: 'column',
-    justifyContent: 'space-around',
-    backgroundColor: '#00000040',
+  modal: {
+    marginHorizontal: 0,
+    marginBottom: 0,
   },
   container: {
-    backgroundColor: '#FFFFFF',
-    padding: 10,
-    height: 100,
-    width: 200,
-    borderRadius: 10,
-    display: 'flex',
+    ...styleDebug('red'),
+    backgroundColor: 'white',
+    padding: 20,
+    flex: 1,
+    marginTop: 400,
+    justifyContent: 'center',
     alignItems: 'center',
-    justifyContent: 'space-between',
-  }
+    borderTopRightRadius: 20,
+    borderTopLeftRadius: 20,
+  },
 });
